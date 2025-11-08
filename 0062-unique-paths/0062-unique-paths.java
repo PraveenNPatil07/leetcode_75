5cf3e593a8874f8cd35a3a31d9e2995b000419a0
Praveen N Patil <138059710+PraveenNPatil07@users.noreply.github.com>
2025-11-08 09:41:03 +0530

    Time: 0 ms (100%), Space: 42.3 MB (6.63%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -2,19 +2,19 @@ class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m+1][n+1];
         for(int[] d : dp) Arrays.fill(d, -1);
-        return yo(m-1, n-1, dp);
+        return yo(0, 0, dp);
     }
 
     public int yo(int m, int n, int[][] dp){
-        if(n == 0 && m == 0){
+        if(n == dp[0].length-2 && m == dp.length - 2){
             dp[m][n] = 1;
              return 1;
         }
-        if(n < 0 || m < 0){
+        if(n >= dp[0].length-1 || m >= dp.length-1){
             return 0;
         }
         if(dp[m][n] != -1) return dp[m][n];
-        dp[m][n] = yo(m-1, n, dp) + yo(m, n-1, dp);
+        dp[m][n] = yo(m+1, n, dp) + yo(m, n+1, dp);
         return dp[m][n];
     }
 }
